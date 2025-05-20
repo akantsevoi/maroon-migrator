856f43686b676e2b9af9da8319bf704115f5c270
Aliaksandr Kantsevoi <aliaksandrkantsevoi@gmail.com>
2025-05-20 15:05:46 +0200

    rename app -> node

--- tests/integration/src/lib.rs
+++ tests/integration/src/lib.rs
@@ -28,7 +28,7 @@ async fn test_some() {
         consensus_nodes: NonZeroUsize::new(2).unwrap(),
     };
 
-    let mut app0 = stack::create_stack(
+    let mut node0 = stack::create_stack(
         vec![
             "/ip4/127.0.0.1/tcp/3001".to_string(),
             "/ip4/127.0.0.1/tcp/3002".to_string(),
@@ -37,7 +37,7 @@ async fn test_some() {
         params.clone(),
     )
     .unwrap();
-    let mut app1 = stack::create_stack(
+    let mut node1 = stack::create_stack(
         vec![
             "/dns4/localhost/tcp/3000".to_string(),
             "/dns4/localhost/tcp/3002".to_string(),
@@ -46,7 +46,7 @@ async fn test_some() {
         params.clone(),
     )
     .unwrap();
-    let mut app2 = stack::create_stack(
+    let mut node2 = stack::create_stack(
         vec![
             "/ip4/127.0.0.1/tcp/3000".to_string(),
             "/ip4/127.0.0.1/tcp/3001".to_string(),
@@ -56,13 +56,13 @@ async fn test_some() {
     )
     .unwrap();
 
-    let mut state_check_0 = app0.get_state_interface();
-    let mut state_check_1 = app1.get_state_interface();
-    let mut state_check_2 = app2.get_state_interface();
+    let mut state_check_0 = node0.get_state_interface();
+    let mut state_check_1 = node1.get_state_interface();
+    let mut state_check_2 = node2.get_state_interface();
 
-    tokio::spawn(async move { app0.loop_until_shutdown(shutdown_rx_0).await });
-    tokio::spawn(async move { app1.loop_until_shutdown(shutdown_rx_1).await });
-    tokio::spawn(async move { app2.loop_until_shutdown(shutdown_rx_2).await });
+    tokio::spawn(async move { node0.loop_until_shutdown(shutdown_rx_0).await });
+    tokio::spawn(async move { node1.loop_until_shutdown(shutdown_rx_1).await });
+    tokio::spawn(async move { node2.loop_until_shutdown(shutdown_rx_2).await });
 
     let mut gw = Gateway::new(vec![
         "/ip4/127.0.0.1/tcp/3000".to_string(),
@@ -87,7 +87,7 @@ async fn test_some() {
         }))
         .await;
 
-    let (mut app0_correct, mut app1_correct, mut app2_correct) = (false, false, false);
+    let (mut node0_correct, mut node1_correct, mut node2_correct) = (false, false, false);
 
     let mut attempts_left = 3;
     let get_state_and_compare = async |interface: &mut ReqResPair<AppRequest, AppResponse>,
@@ -105,16 +105,16 @@ async fn test_some() {
         consensus_offset: HashMap::from([(KeyRange(0), KeyOffset(1))]),
     };
 
-    while attempts_left > 0 || !(app0_correct && app1_correct && app2_correct) {
-        app0_correct = get_state_and_compare(&mut state_check_0, &desired_state).await;
-        app1_correct = get_state_and_compare(&mut state_check_1, &desired_state).await;
-        app2_correct = get_state_and_compare(&mut state_check_2, &desired_state).await;
+    while attempts_left > 0 || !(node0_correct && node1_correct && node2_correct) {
+        node0_correct = get_state_and_compare(&mut state_check_0, &desired_state).await;
+        node1_correct = get_state_and_compare(&mut state_check_1, &desired_state).await;
+        node2_correct = get_state_and_compare(&mut state_check_2, &desired_state).await;
 
         tokio::time::sleep(Duration::from_secs(1)).await;
         attempts_left -= 1;
     }
 
-    assert!(app0_correct);
-    assert!(app1_correct);
-    assert!(app2_correct);
+    assert!(node0_correct);
+    assert!(node1_correct);
+    assert!(node2_correct);
 }
